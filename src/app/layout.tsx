6d82bf4351247d109e5c8efd0cbc3bcadaa3b941
Neto Moraes <jomoraesneto@gmail.com>
2024-05-13 00:06:33 -0300

    general update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import logo from "../../public/assets/Logo_OCTOBIT_Logo_Branco_SIMBOLO.png"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="fixed w-[100%] h-24 opacity-1 bg-[var(--black)]">
-        
-        </header>
+        <Header/>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import logo_simbolo_PRETO from "../../public/assets/Logo_OCTOBIT_Logo_Preto_SIMBOLO.png";
 
 export default function Home() {
   return (
     <main>
+      
+      <Image src={logo_simbolo_PRETO} alt="logo" />
+      <p className="font-bold text-[2rem]">
+        A OctoBit Jr. é uma empresa júnior fundada e composta majoritariamente por alunos
+        do curso de Bacharelado em Ciência da Computação, da FFCLRP-USP. Ela
+        realiza serviços sem fins lucrativos, proporcionando aos membros experiências
+        profissionais desde que ingressam no curso, até a sua conclusão.
+      </p>
+
     </main>
   );
 }
